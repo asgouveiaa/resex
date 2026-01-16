527a38db387316c95bd76aebf32a655364ea347b
Ayrton Gouveia <ayrtonsgouveia19@gmail.com>
2026-01-16 09:41:27 -0300

    updated

--- tidy_resex.R
+++ tidy_resex.R
@@ -6,6 +6,10 @@ file_path <- "./PLANILHA_MATRIZES_ANALISE_RISCO_RESEX.xlsx"
 all_sheets <- excel_sheets("./PLANILHA_MATRIZES_ANALISE_RISCO_RESEX.xlsx")
 
 list_of_tables <- map(all_sheets, ~ read_excel(file_path, sheet = .x))
+list_of_tables$Caça <- list_of_tables$Caça %>% 
+  rename(OUTROS = CPOUTROS,
+         QUEIMA = CPQUEIMA)
+
 names(list_of_tables) <- all_sheets # Assign sheet names to list elements
 
 ##
@@ -13,16 +17,32 @@ names(list_of_tables) <- all_sheets # Assign sheet names to list elements
 resex <- list_of_tables[-1] %>%
   reduce(full_join, by = "Amostra")
 
-resex$Comunidade %>% unique() #35 comunidades
-resex$Amostra %>% unique()  #567 amostras (famílias)
+resex$Comunidade %>% n_distinct() 
+resex$Comunidade %>% unique()
+
+
+resex$Amostra %>% n_distinct()  
+resex$Amostra %>% unique()
 
 resex %>% group_by(Comunidade) %>% 
   summarize(n_amostras = length(Amostra)) %>% View()
 
 
-names(resex)
+names(list_of_tables$Atividades)
+names(list_of_tables$Saúde)
+names(list_of_tables$Caça)
+names(list_of_tables$Animais)
 
-View(resex)
+#CF sim = caçado por alguém família
+#LP = quem limpa
+#PC = problemas na carne
+#chum = chumbo da bala
+#QA = quem come a carne infectada descartada
+
+#cl = chegam longe
+#cldo = chegam de longe doentes
+#OA = outros animais que passam doença segundo os entrevistados
+
 
 ## perguntas
 
@@ -90,7 +110,7 @@ vac_para <- c("QVSAUMEG","QVAFTOSA","QVBOUBA","QVBRUCEL","QVVERDEX","QVOUTRAS",
               "QVVERME","VC_CALAZ","VC_VITAMIN")
 
 nao_identificadas <- c("VQD2013","VQD2012","VQD2010","VQD2015","VQD2014","VQDNOLEMB",
-                       "UV_2016")
+                       "UV_2016", "JABECTO","MACACINT","PASSARIND")
 
 
 caes_vac_para <- c("CAOENDOP","CAOECTOP","CAOVIRUS","CAOBACTER","CAOLVC",
@@ -104,18 +124,70 @@ boi_vac_para <- c("GALVIRUS","GALBOURBA","GALMAREK","GALNEWCA",
                        "GALPARAS","GALIFBAC","GALCORIZ","GALCEGU",
                        "GALINDET","GALINTOX","GALNEOP")
 
+gato_vac_para <- c("GATVIRUS", "GATPARAS","GATBACT","GATALERG","GATOIND")
+
+pato_vac_para <- c("PATVIRUS","PATPARAS","PATBACT","PATNUTRI")
+
+porco_vac_para <- c("PORCENDO","PORCECTO","PORCIND")
+
+uso_alcool <- c("HALCOOL")
+
+problemas_saude_homem <- names(list_of_tables$Saúde)[startsWith(names(list_of_tables$Saúde), "H")]
+
+problemas_saude_mulher <- names(list_of_tables$Saúde)[startsWith(names(list_of_tables$Saúde), "M")]
+
+problemas_saude_idoso <- names(list_of_tables$Saúde)[startsWith(names(list_of_tables$Saúde), "I")]
+
+problemas_saude_crianc <- names(list_of_tables$Saúde)[startsWith(names(list_of_tables$Saúde), "C")]
+
+# CAÇA
+
+carne_musada <- names(list_of_tables$Caça)[startsWith(names(list_of_tables$Caça), "MU")]
+
+caça_preferida <-  names(list_of_tables$Caça)[startsWith(names(list_of_tables$Caça), "CP")]
+
+freqcons_caça <-  names(list_of_tables$Caça)[startsWith(names(list_of_tables$Caça), "FC")]
+
+como_consome_caça <- names(list_of_tables$Caça)[startsWith(names(list_of_tables$Caça), "CC")]
+
+quem_limpa_caça <- names(list_of_tables$Caça)[startsWith(names(list_of_tables$Caça), "CC")]
+
+problema_carne_caça <- names(list_of_tables$Caça)[startsWith(names(list_of_tables$Caça), "PC")]
+
+problemas_encontrados_caça <- c("Alt_Carn", "CARO_FIG", "CHUMBO",
+                                "FERIDA","MAGRA", "MANCH_BR","MICROB",
+                                "NAO_DET","SINT_DO","SINT_INF","PARASIT",
+                                "PERD_PEL","REMOSA","TUMOR","URA","VERMES")
+
+caça_relatada_problema <- c("ANTA","BOI","ARAÇU","BOZO",
+                            "CACHO","CAITI","CAPIV","CARAT",
+                            "COELH","CUTIA","GALI","GATO","GUARI",
+                            "JABUT","jacamin","acu","ACUN","JAVAL",
+                            "MACAC","MACBG","MACPREG","MCVLH","MUCUR",
+                            "MUTUM","ONÇA","ONCPI","OURIÇ","OUTRO","PACA",
+                            "PEIXE","piranh","PXBOI","PESCA","PORCO","PORMA",
+                            "PREGU","QUATI","QUEIX","RAPOS","TAMAN","TATU",
+                            "TODOS","TRAIR","TUCUN","VEADO")
 
 
+destino_caça_impropria <- c("COZINCOM","ANIMCOME","DEIXAMAT","ENTERRA","ESCALDA",
+                            "JOGALIXO","JOGAMATO","NAOCOME","CPOUTROS","CPQUEIMA",
+                            "TIRPEDAC")
+
+animal_come_caça_impropria <- names(list_of_tables$Caça)[startsWith(names(list_of_tables$Caça), "QA")]
 
 
+# ANIMAIS
 
+animais_nunca_problemas <- names(list_of_tables$Animais)[startsWith(names(list_of_tables$Animais), "NP")]
 
+animais_sempre_problemas <- names(list_of_tables$Animais)[startsWith(names(list_of_tables$Animais), "SP")]
 
+animais_epoca_problemas <- names(list_of_tables$Animais)[startsWith(names(list_of_tables$Animais), "EDQ")]
 
+animais_epoca_problemas <- names(list_of_tables$Animais)[startsWith(names(list_of_tables$Animais), "EDQ")]
 
+animais_problemas <- names(list_of_tables$Animais)[startsWith(names(list_of_tables$Animais), "EDP")]
 
-
-
-
-
+animais_chegam_longe <- names(list_of_tables$Animais)[startsWith(names(list_of_tables$Animais), "CL")]
 
